096658f020852b96b3a2b7d828a2e88f888eb25b
Shangfeng Liu <sliu@tyro.com>
2019-08-30 17:14:17 +0800

    --amend

--- .buildkite/scripts/parse.sh
+++ .buildkite/scripts/parse.sh
@@ -11,3 +11,6 @@ echo "Start"
 jq -R 'split(",")' <<< ${CSV_FILE} | jq '{requestType: .[0], registerNumber: .[1], storeId: .[2], storeAddress: .[3]}' >> temp
 
 echo "end"
+
+echo "Cat temp"
+cat temp
