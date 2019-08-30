bbe375b2a62ef9b275efce3b404a0a769c45ca96
Shangfeng Liu <sliu@tyro.com>
2019-08-30 17:16:49 +0800

    Ignor inputs

--- .buildkite/scripts/parse.sh
+++ .buildkite/scripts/parse.sh
@@ -8,7 +8,7 @@ echo -e ${CSV_FILE}
 
 echo "Start"
 
-jq -R 'split(",")' <<< ${CSV_FILE} | jq '{requestType: .[0], registerNumber: .[1], storeId: .[2], storeAddress: .[3]}' >> temp
+jq -R 'split(",")' <<< ${CSV_FILE} | jq 'inputs | {requestType: .[0], registerNumber: .[1], storeId: .[2], storeAddress: .[3]}' >> temp
 
 echo "end"
 
