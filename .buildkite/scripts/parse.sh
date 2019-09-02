b70748d0f90caccfb385676027393eafd42398de
Shangfeng Liu <sliu@tyro.com>
2019-09-02 14:04:59 +0800

    Fix the double quotes issue when parsing csv file.

--- .buildkite/scripts/parse.sh
+++ .buildkite/scripts/parse.sh
@@ -6,31 +6,33 @@ set -euo pipefail
 #Color constants
 readonly var RED='\033[0;31m'
 readonly var GREEN='\033[0;32m'
+readonly var YELLOW='\033[0;33m'
 readonly var NC='\033[0m'
 
 # Set the URL of csv file.
-CSV_FILE_NAME="CSV_FILE.csv"
+CSV_FILE_URL="CSV_FILE.csv"
 
-echo "Retrieve CSV file from S3:"
+echo "Retrieve CSV file from S3: ${CSV_FILE_URL}"
 # aws s3 ....  CSV_FILE_URL
-printf "${GREEN}Retrieve successfully${NC}\n"
+printf "${GREEN}Retrieved successfully${NC}\n"
 
-echo "Start parsing and sending message to SQS.... (Refactor the file name with variable)"
-caids_data=`jq -R 'split(",")' "${CSV_FILE_NAME}"  \
+CSV_DATA=`cat ${CSV_FILE_URL} | sed -e 's/^"//' -e 's/"$//'`
+
+echo "************** Start Parsing and Sending ***************"
+echo "Start parsing and sending message to SQS"
+caids=`jq -R 'split("\",\"")' <<< "${CSV_DATA}"  \
  | jq '[inputs | {requestType: .[0], registerNumber: .[1], storeId: .[2], storeAddress: .[3]}]'`
 
-echo "caids_data:"
-echo ${caids_data}
 
-echo "length:"
-caids_size=`echo ${caids_data} | jq '. | length'`
+caids_size=`echo ${caids} | jq '. | length'`
+printf "${YELLOW}Caids count: ${caids_size}${NC}\n"
 
-printf "${RED}Sending message to SQS queue.${NC}\n"
-jq -c '.[]' <<< "${caids_data}" | while read caid; do
+jq -c '.[]' <<< "${caids}" | while read caid; do
     echo "caid: "${caid}
 
     # send to SQS
 
 done
+echo "************** Finished Parsing and Sending ***************"
 
 printf "${GREEN}Finished parsing and sending message to SQS successfully.${NC}\n"
