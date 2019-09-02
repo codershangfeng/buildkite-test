7a6e422aceffe4d0b0dfd5fefa6c7c0dd77da548
Shangfeng Liu <sliu@tyro.com>
2019-09-02 10:07:44 +0800

    Add double quotes

--- .buildkite/scripts/parse.sh
+++ .buildkite/scripts/parse.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 echo "CSV_FILE:"
-echo -e ${CSV_FILE}
+echo "${CSV_FILE}"
 
 
 echo "Start"
 
-jq -R 'split(",")' <<< ${CSV_FILE} | jq 'inputs | {requestType: .[0], registerNumber: .[1], storeId: .[2], storeAddress: .[3]}' >> temp
+jq -R 'split(",")' <<< "${CSV_FILE}" | jq 'inputs | {requestType: .[0], registerNumber: .[1], storeId: .[2], storeAddress: .[3]}' >> temp
 
 echo "end"
 
